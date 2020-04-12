e9535a2149a99fb71acb7c55bd226d5f93ca3554
Marcelo Moraes <22119544+marcelomdantas@users.noreply.github.com>
2020-04-12 20:06:11 -0300

    adicionei arquivos via upload

---
+++ init.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$('.nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
+
+
